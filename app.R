001a301f211ae6217b9aa42dc07906a008fe426e
Nathan Wong <cloudynathan@gmail.com>
2020-01-01 15:50:21 -0800

    shinydashboard tabs

--- app.R
+++ app.R
@@ -4,8 +4,35 @@
 
 #load packages
 library(shiny)
+library(shinydashboard)
 library(tidyverse)
 
 #import data
 df <- read.csv("Car_sales.csv", stringsAsFactors = FALSE)
 
+#app
+ui <- dashboardPage(
+        dashboardHeader(title = "Car sales"),
+        
+        dashboardSidebar(
+          sidebarMenu(
+            menuItem("Page1", tabName = "page1", icon = icon("folder")),
+            menuItem("Page2", tabName = "page2", icon = icon("folder"))
+          )
+        ),
+        
+        dashboardBody(
+          tabItems(
+            #page1 content
+            tabItem(tabName = "page1"),
+            
+            #page2 content
+            tabItem(tabName = "page2")
+            
+          )
+        )
+)
+
+server <- function(input, output) {}
+
+shinyApp(ui = ui, server = server)
