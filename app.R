6b7557be440390ebc176fe01158ac19aecff5362
Nathan Wong <cloudynathan@gmail.com>
2020-01-01 15:33:00 -0800

    first commit

---
+++ app.R
@@ -0,0 +1,11 @@
+###################
+# shiny car sales #
+###################
+
+#load packages
+library(shiny)
+library(tidyverse)
+
+#import data
+df <- read.csv("Car_sales.csv", stringsAsFactors = FALSE)
+
